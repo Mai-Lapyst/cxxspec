7d9e7a409e20769e04256222a8798f52944ad884
Mai-Lapyst <info@lapyst.by>
2021-12-25 16:37:17 +0100

    Implementing first version of junit formatter

---
+++ src/formatters/junit_formatter.cpp
@@ -0,0 +1,65 @@
+#include "./junit_formatter.hpp"
+
+#include <ctime>
+
+namespace cxxspec {
+
+    void JunitFormatter::onBeginTesting() {}
+    void JunitFormatter::onEndTesting() {
+        auto old_precision = stream.precision();
+        stream.precision(9);
+        stream << std::fixed;
+
+        stream << "<?xml version=\"1.0\" encoding=\"utf-8\"?>" << endl;
+        stream << "<testsuite";
+            stream << " errors=\"0\"";
+            stream << " failures=\"" << this->failures << "\"";
+            stream << " skipped=\"0\"";
+            stream << " tests=\"" << this->testcases.size() << "\"";
+            JunitDuration time = std::chrono::duration_cast<JunitDuration>(this->timeSum);
+            stream << " time=\"" << time.count() << "\"";
+        stream << ">" << endl;
+        chi(1);
+
+        for (JunitTestcase& testcase : this->testcases) {
+            i(); stream << "<testcase";
+                stream << " classname=\"<cpp-file>\"";
+                stream << " name=\"" << testcase.name << "\"";
+                stream << " time=\"" << testcase.timeTaken.count() << "\"";
+            if (testcase.result) {
+                stream << "/>" << endl;
+            }
+            else {
+                stream << ">" << endl;
+                chi(1);
+                    i(); stream << "<failure";
+                        stream << " message=\"" << testcase.reason << "\"";
+                        stream << " type=\"expect\"";
+                        stream << ">" << endl;
+                chi(-1);
+                i(); stream << "</testcase>" << endl;
+            }
+        }
+
+        chi(-1);
+        stream << "</testsuite>" << endl;
+
+        stream.precision(old_precision);
+        stream << std::defaultfloat;
+    }
+
+    void JunitFormatter::onEnterSpec(Spec& spec) {}
+    void JunitFormatter::onLeaveSpec(Spec& spec, bool hasNextElement) {}
+
+    void JunitFormatter::onEnterExample(Example& example) {}
+    void JunitFormatter::onExampleResult(Example& example, bool result, std::string reason, ExampleDuration timeTaken) {
+        JunitDuration time = std::chrono::duration_cast<JunitDuration>(timeTaken);
+        testcases.push_back(JunitTestcase(example.fullname(), result, reason, time));
+        timeSum += timeTaken;
+        if (!result) {
+            failures++;
+        }
+    }
+    void JunitFormatter::onLeaveExample(Example& example, bool hasNextElement) {}
+
+}
---
+++ src/formatters/junit_formatter.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "../core/core.hpp"
+#include "./prettyable_formatter.hpp"
+
+#include <ostream>
+
+namespace cxxspec {
+    typedef std::chrono::duration<double> JunitDuration;
+
+    class JunitTestcase {
+    private:
+        std::string name;
+        JunitDuration timeTaken;
+        bool result;
+        std::string reason;
+
+        friend class JunitFormatter;
+    public:
+        JunitTestcase(std::string name, bool result, std::string reason, JunitDuration timeTaken)
+            : name(name), result(result), reason(reason), timeTaken(timeTaken)
+        {}
+    };
+
+    class JunitFormatter : public PrettyableFormatter {
+    protected:
+        std::vector<JunitTestcase> testcases;
+        ExampleDuration timeSum;
+        std::size_t failures = 0;
+
+    public:
+        JunitFormatter(std::ostream& stream, bool pretty = true) : PrettyableFormatter(stream, pretty) {}
+
+        void onBeginTesting();
+        void onEndTesting();
+
+        void onEnterSpec(Spec& spec);
+        void onLeaveSpec(Spec& spec, bool hasNextElement);
+
+        void onEnterExample(Example& example);
+        void onExampleResult(Example& example, bool result, std::string reason, ExampleDuration timeTaken);
+        void onLeaveExample(Example& example, bool hasNextElement);
+    };
+}
