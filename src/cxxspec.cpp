f5c4e1c5186df8d9af9f547359adfdca7f4b4d73
Mai-Lapyst <hello@lapyst.dev>
2024-08-20 05:14:59 +0200

    Add license headers to sourcefiles

--- src/cxxspec.cpp
+++ src/cxxspec.cpp
@@ -1,3 +1,21 @@
+/*
+ * cxxspec - a TDD/BDD framework for c++ projects
+ * Copyright (C) 2021-2024 Mai-Lapyst
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "./cxxspec.hpp"
 #include "./formatters/cli_formatter.hpp"
 #include "./formatters/json_formatter.hpp"
